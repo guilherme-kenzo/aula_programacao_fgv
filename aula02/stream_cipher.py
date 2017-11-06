39b3fbbd88c5d612f5a7e85246c3b8b0773dcf7f
guilherme_kenzo <guikenzo.s@gmail.com>
2017-11-06 13:39:22 -0200

    adicionando pasta da segunda aula

---
+++ aula02/stream_cipher.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import random
+from string import printable
+
+def open_and_read(file_name):
+    f = open(file_name)
+    plaintext = f.read()
+    return plaintext
+
+def create_key(user_key):
+
+
+
+if __name__ == '__main__':
+    print(open_and_read('plain.txt'))
